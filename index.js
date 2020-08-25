17923b59e938705075bcf0f885608a0637a763f9
vchandu111 <vchanduu111@gmail.com>
2020-08-25 10:28:47 +0530

    Portfolio

--- index.js
+++ index.js
@@ -5,30 +5,33 @@ AOS.init({
   },
 });
 
-const counters = document.querySelectorAll(".counter");
-const speed = 100; // The lower the slower
+let counter_up = document.querySelector(".counter-up");
+counter_up.addEventListener("mouseover", () => {
+  const counters = document.querySelectorAll(".counter");
+  const speed = 100; // The lower the slower
 
-counters.forEach((counter) => {
-  const updateCount = () => {
-    const target = +counter.getAttribute("data-target");
-    const count = +counter.innerText;
+  counters.forEach((counter) => {
+    const updateCount = () => {
+      const target = +counter.getAttribute("data-target");
+      const count = +counter.innerText;
 
-    // Lower inc to slow and higher to slow
-    const inc = Math.floor(target / speed);
+      // Lower inc to slow and higher to slow
+      const inc = Math.floor(target / speed);
 
-    // console.log(inc);
-    // console.log(count);
+      // console.log(inc);
+      // console.log(count);
 
-    // Check if target is reached
-    if (count < target) {
-      // Add inc to count and output in counter
-      counter.innerText = count + inc;
-      // Call function every ms
-      setTimeout(updateCount, 200);
-    } else {
-      counter.innerText = `${target}+`;
-    }
-  };
+      // Check if target is reached
+      if (count < target) {
+        // Add inc to count and output in counter
+        counter.innerText = count + inc;
+        // Call function every ms
+        setTimeout(updateCount, 200);
+      } else {
+        counter.innerText = `${target}+`;
+      }
+    };
 
-  updateCount();
+    updateCount();
+  });
 });
